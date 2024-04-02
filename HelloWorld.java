20a21cae9b6ea582cd82f3cf50d5415a1c8be036
Ansible <ansible@ansible-c.example.com>
2024-04-03 02:03:07 +0530

    logger.log added inplace of system.out

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, world! PipeLine");
+        //System.out.println("Hello, world! PipeLine");
+	logger.log("Hello, world!");
     }
 }
 
