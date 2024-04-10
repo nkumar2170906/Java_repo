636d918e7e463ecda6277c696264b964d703fc8a
Ansible <ansible@ansible-c.example.com>
2024-04-10 23:22:25 +0530

    Modified new files deploy-war.sh

--- deploy-war.sh
+++ deploy-war.sh
@@ -1,10 +1,9 @@
-echo
-echo " Deploying the war file to Tomcat Server"
-echo
+node{
 
-sshagent(['tomcat-dev']) {
-    sh 'scp -o StrictHostKeyChecking=no dist/*.war ec2-user@ 3.110.44.181:/opt/tomcat/webapps/'
+   stage('Deploy to Tomcat'){
+   
+      sshagent(['tomcat-dev']) {
+         sh 'scp -o StrictHostKeyChecking=no dist/*.war ec2-user@3.110.44.181:/opt/tomcat/webapps/'
+	}
+    }		
 }
-
-echo "Done"
-echo
