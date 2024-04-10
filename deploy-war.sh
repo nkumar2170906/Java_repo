6a55692bf85c0001347db22855598b6337a14d5d
Ansible <ansible@ansible-c.example.com>
2024-04-10 23:33:52 +0530

    Modified again new files deploy-war.sh

--- deploy-war.sh
+++ deploy-war.sh
@@ -1,9 +1,17 @@
-node{
+##node{
+##
+##   stage('Deploy to Tomcat'){
+##   
+##      sshagent(['tomcat-dev']) {
+##         sh 'scp -o StrictHostKeyChecking=no dist/*.war ec2-user@3.110.44.181:/opt/tomcat/webapps/'
+##	}
+##    }		
+##}
 
-   stage('Deploy to Tomcat'){
-   
-      sshagent(['tomcat-dev']) {
-         sh 'scp -o StrictHostKeyChecking=no dist/*.war ec2-user@3.110.44.181:/opt/tomcat/webapps/'
-	}
-    }		
-}
+echo "Deploying war file to Tomcat Server"
+echo
+   sudo scp -i /root/ec2-tomcat-keypair.pem -o StrictHostKeyChecking=no dist/*.war ec2-user@3.110.44.181:/opt/tomcat/webapps
+echo
+echo "Done"
+
+
