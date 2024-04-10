76303fe170a83f8325f6429270fe28fc672075f7
Ansible <ansible@ansible-c.example.com>
2024-04-11 01:22:46 +0530

    Edited deploy-war.sh

--- deploy-war.sh
+++ deploy-war.sh
@@ -10,7 +10,7 @@
 
 echo "Deploying war file to Tomcat Server"
 echo
-   #sudo scp -i /root/ec2-tomcat-keypair.pem -o StrictHostKeyChecking=no dist/*.war ec2-user@3.110.44.181:/opt/tomcat/webapps
+   sudo scp -i /root/ec2-tomcat-keypair.pem -o StrictHostKeyChecking=no dist/*.war ec2-user@3.110.44.181:/opt/tomcat/webapps
 echo
 echo "Done"
 
